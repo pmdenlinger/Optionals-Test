745fdb09df014e09bfb0fdd2c7c4b2a13e927c71
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-18 19:09:55 -0700

    Initial Commit

---
+++ Optionals Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Optionals Test
+//
+//  Created by user on 5/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Optionals Test/Optionals_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Optionals_TestApp.swift
+//  Optionals Test
+//
+//  Created by user on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Optionals_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
